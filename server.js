3a0f04e8253b0642beb88676fda5fc8a045852ca
McKenzie Cottrell <mckenziecottrell@gmail.com>
2021-11-20 22:26:44 -0500

    fixed error with connecting to database

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
-const path = require("path");
-const express = require("express");
-const session = require("express-session");
-const exphbs = require("express-handlebars");
-const routes = require("./controllers");
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 
-const sequelize = require("./config/connection");
-
-// Create a new sequelize store using the express-session package
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create();
 
-// Configure and link a session object with the sequelize store
 const sess = {
-  secret: "Super secret secret",
+  secret: 'Super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -25,15 +22,14 @@ const sess = {
   }),
 };
 
-// Add express-session and store as Express.js middleware
 app.use(session(sess));
 
-app.engine("handlebars", exphbs.engine());
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
