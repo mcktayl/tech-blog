6eb6b3851bfb9296befb8aab9f3dd53fefef40cc
McKenzie Cottrell <mckenziecottrell@gmail.com>
2021-11-27 16:26:25 -0500

    added login javascript functionality

---
+++ public/js/login.js
@@ -0,0 +1,28 @@
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  // gather user data from the form elements on the page
+  const email = document.querySelector('#exampleInputEmail1').value.trim();
+  const password = document
+    .querySelector('#exampleInputPassword1')
+    .value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+      console.log(response);
+    } else {
+      alert('Failed to log in');
+    }
+  }
+};
+console.log('connected');
+document
+  .querySelector('#login-form')
+  .addEventListener('submit', loginFormHandler);
